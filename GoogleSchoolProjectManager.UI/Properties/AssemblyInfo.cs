c89c4e063940e5eed794f2dae5cf872782fd4f99
Jan Urbanec <UrbanecJ@seznam.cz>
2022-10-30 19:56:12 +0100

    Changed versioning of the assembly to relate with Date, cleaned up assembly files. Version: 2022.10.30.0001

--- GoogleSchoolProjectManager.UI/Properties/AssemblyInfo.cs
+++ GoogleSchoolProjectManager.UI/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
@@ -8,13 +6,8 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GoogleSchoolProjectManager.UI")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Jan Urbanec")]
-[assembly: AssemblyProduct("GoogleSchoolProjectManager.UI")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("Hosting part + UI of the Google drive manager app.")]
+
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -39,17 +32,3 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
-
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
---
+++ Properties/GlobalAssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Windows;
+
+#if (DEBUG)
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyCompany("Jan Urbanec")]
+[assembly: AssemblyProduct("Google - School Project Manager")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: AssemblyCulture("")]
+
+//In order to begin building localizable applications, set
+//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
+//inside a <PropertyGroup>.  For example, if you are using US english
+//in your source files, set the <UICulture> to en-US.  Then uncomment
+//the NeutralResourceLanguage attribute below.  Update the "en-US" in
+//the line below to match the UICulture setting in the project file.
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+
+
+// Version information for an assembly consists of the following four values:
+// roughly translated from I reckon it is for SO, note that they most likely 
+// dynamically generate this file
+//      Major Version  - Year 6 being 2016
+//      Minor Version  - The month
+//      Day Number     - Day of month
+//      Revision       - Build number
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below: [assembly: AssemblyVersion("year.month.day.*")]
+[assembly: AssemblyVersion("2022.10.00.00")]
+[assembly: AssemblyFileVersion("2022.10.30.0001")]
